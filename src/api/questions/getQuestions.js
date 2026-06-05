e6fa1b86f37a2d4b29ae9f86c207789a195e93ff
k.leshchuk <k.leshchuk@mail.astondevs.ru>
2026-06-05 19:29:22 +0400

    feat: add useDebounce hook and search param in API

--- src/api/questions/getQuestions.js
+++ src/api/questions/getQuestions.js
@@ -1,9 +1,15 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
-export async function getQuestions({page = 1, limit = 10} = {}) {
+export async function getQuestions({ page = 1, limit = 10, search = "" } = {}) {
   const params = new URLSearchParams({ page, limit });
 
-  const response = await fetch(`${API_URL}/questions/public-questions?${params}`);
+  if (search) {
+    params.append("titleOrDescription", search);
+  }
+
+  const response = await fetch(
+    `${API_URL}/questions/public-questions?${params}`,
+  );
   if (!response.ok) {
     throw new Error(`Error: ${response.status}`);
   }
