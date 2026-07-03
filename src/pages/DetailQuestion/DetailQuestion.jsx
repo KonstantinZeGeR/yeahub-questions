46fb2a422dc4004c62f4a1fa731d12357d75e7ad
k.leshchuk <k.leshchuk@mail.astondevs.ru>
2026-07-03 15:25:48 +0400

    feat: question detail page  and basic styles

--- src/pages/DetailQuestion/DetailQuestion.jsx
+++ src/pages/DetailQuestion/DetailQuestion.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import { getPublicQuestionById } from "../../api/questions/getPublicQuestionById";
+import styles from "./DetailQuestion.module.css";
 
 export function DetailQuestion() {
   const { id } = useParams();
@@ -15,10 +16,41 @@ export function DetailQuestion() {
   if (!question) return null;
 
   return (
-    <div>
-      <Link to="/">К списку вопросов</Link>
+    <div className={styles.page}>
+      <Link to="/" className={styles.back}>
+        К списку вопросов
+      </Link>
+      {question.imageSrc && (
+        <img src={question.imageSrc} alt="question" width={80} />
+      )}{" "}
       <h1>{question.title}</h1>
       <p>{question.description}</p>
+      <p>Сложность: {question.complexity}</p>
+      <p>Рейтинг: {question.rate}</p>
+      <p>Навыки:</p>
+      <ul className={styles.tags}>
+        {question.questionSkills.map((skill) => (
+          <li className={styles.tag} key={skill.id}>
+            {skill.title}
+          </li>
+        ))}
+      </ul>
+      <p>Ключевые слова:</p>
+      <ul className={styles.tags}>
+        {question.keywords.map((keyword) => (
+          <li className={styles.tag} key={keyword}>
+            #{keyword}
+          </li>
+        ))}
+      </ul>
+      <p>Автор: {question.createdBy?.username}</p>
+      <div className={styles.card}>
+        <h2>Краткий ответ</h2>
+        <div dangerouslySetInnerHTML={{ __html: question.shortAnswer }} />
+
+        <h2>Развёрнутый ответ</h2>
+        <div dangerouslySetInnerHTML={{ __html: question.longAnswer }} />
+      </div>
     </div>
   );
 }
